29eb312803de0ca8c435c039423a219512eb5de7
Gergely <gergely.tarsoly3@gmail.com>
2018-04-15 19:24:39 +0200

    Add camera calibration input from file

--- src/util/cam_calibration.cpp
+++ src/util/cam_calibration.cpp
@@ -11,15 +11,15 @@ void read_mat3(std::istream &in, glm::mat3 &m)
     }
 }
 
-void print_mat3(const glm::mat3 &m)
+void print_mat3(std::ostream &os, const glm::mat3 &m)
 {
     for(int i = 0; i < 3; ++i)
     {
         for(int j = 0; j < 3; ++j)
         {
-            std::cout << m[j][i] << " ";
+            os << m[j][i] << " ";
         }
-        std::cout << "\n";
+        os << "\n";
     }
 }
 
@@ -39,85 +39,144 @@ glm::vec3 MatVec::mult_add(const glm::vec3 &v)
     return is_valid ? glm::vec3{R * v + t} : glm::vec3{};
 }
 
+ImageCalibration::ImageCalibration(int num, int size) : num { num }
+{
+    mat_vecs.resize(size);
+}
+
 CamCalibration::CamCalibration(int size)
 {
     image_calibrations.reserve(size);
 }
 
-void CamCalibration::add_image_calibration(const ImageCalibration &image_cal)
+std::istream& operator>>(std::istream &in, ImageCalibration &im)
 {
-    image_calibrations.push_back(image_cal);
-}
-
-void CamCalibration::set_Rt(glm::mat3 R_, glm::vec3 t_)
-{
-    lidar1_to_lidar2 = MatVec(R_, t_);
-}
-
-/*
-int main()
-{
-    std::ifstream in{"../../data1/calibration.txt"};
-    std::string lidar, name, eq;
-    int i = 0;
-
-    glm::mat3 R;
-    glm::vec3 t;
-
-    in >> eq;
-    while(eq != "Cam")
-    {
-        if(eq == "=")
-        {
-            if(name == "R")
-            {
-                read_mat3(in, R);
-            }
-            if(name == "t")
-            {
-                read_vec(in, t, 3);
-                break;
-            }
-        }
-
-        name = std::move(eq);
-        in >> eq;
-    }
-
-    print_mat3(R);
-    print_vec(t, 3);
-
-    in >> i;
-
-    name = std::move(eq);
-
+    std::string eq, name;
     while(in >> eq)
     {
         if(eq == "=")
         {
             if (name == "cammatrix")
-            {}
+            {
+                read_mat3(in, im.cam_mat);
+            }
             else if(name == "discoeff")
-            {}
-            else if(name == "R")
-            {}
-            else if(name == "t")
-            {}
-            std::cout << lidar << " " << name << eq << "\n";
+            {
+                read_vec(in, im.discoeff, 5);
+            }
         }
-        else if(eq == "Cam")
+        else if(eq == "lidar1")
         {
-            
+            in >> im.mat_vecs[0];
         }
-        
-
-        //std::cout << i++ << ". : " << eq << "\n";
-
-        lidar = std::move(name);
+        else if(eq == "lidar2")
+        {
+            in >> im.mat_vecs[1];
+        }
+        else if(eq == "next")
+        {
+            break;
+        }
         name = std::move(eq);
     }
 
-    std::cout << eq;
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &os, const ImageCalibration &im)
+{
+    os << "camera matrix: ";
+    print_mat3(os, im.cam_mat);
+    os << "discoeff: ";
+    print_vec(os, im.discoeff, 5);
+
+    for(const auto &mv : im.mat_vecs)
+    {
+        os << mv << "\n";
+    }
+
+    return os;
+}
+
+std::istream& operator>>(std::istream &in, CamCalibration &cam)
+{
+    std::string name, eq;
+    in >> cam.lidar1_to_lidar2;
+
+    while(in >> eq)
+    {
+        if(eq == "Cam")
+        {
+            int num;
+            in >> num;
+            ImageCalibration im(num, 2);
+            in >> im;
+            cam.image_calibrations.push_back(im);
+        }
+    }
+
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &os, const CamCalibration &cam)
+{
+    os << "lidar1 to lidar 2: ";
+    os << cam.lidar1_to_lidar2;
+    for(int i = 0; i < cam.image_calibrations.size(); ++i)
+    {
+        os << "cam nr " << i << ":\n " << cam.image_calibrations[i]; 
+    }
+
+    return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const MatVec &mv)
+{
+    if(mv.is_valid)
+    {
+        os << "R: ";
+        print_mat3(os, mv.R);
+        os << "t: ";
+        print_vec(os, mv.t, 3);
+    }
+
+    return os;
+}
+
+std::istream &operator>>(std::istream &in, MatVec &mv)
+{
+    std::string eq, name;
+    while(in >> eq)
+    {
+        if(eq == "=")
+        {
+            if(name == "R")
+            {
+                read_mat3(in, mv.R);
+            }
+            if(name == "t")
+            {
+                read_vec(in, mv.t, 3);
+                break;
+            }
+        }
+
+        name = std::move(eq);
+    }
+
+    mv.is_valid = true;
+
+    return in;
+}
+
+int main()
+{
+    std::ifstream in{"../../data1/calibration.txt"};
+    CamCalibration cam(6);
+
+    in >> cam;
+
+    std::cout << cam;
 
     return 0;
-}*/
+}
--- src/util/cam_calibration.h
+++ src/util/cam_calibration.h
@@ -8,65 +8,68 @@
 
 #include <glm/glm.hpp>
 
-class MatVec
+struct MatVec
 {
-public:
     MatVec();
     MatVec(const glm::mat3 &R_, const glm::vec3 &t_);
     glm::vec3 mult_add(const glm::vec3 &v);
 
-//private:
     glm::mat3 R;
     glm::vec3 t;
     bool is_valid;
 };
 
-class ImageCalibration
+struct ImageCalibration
 {
-public:
-    ImageCalibration(int num);
+    explicit ImageCalibration(int num, int size);
 
-private:
     int num;
     glm::mat3 cam_mat;
     std::array<float, 5> discoeff;
     std::vector<MatVec> mat_vecs;
 };
 
-class CamCalibration
+struct CamCalibration
 {
-    CamCalibration(int size);
-    void add_image_calibration(const ImageCalibration &image_cal);
-    void set_Rt(glm::mat3 R_, glm::vec3 t_);
+    CamCalibration();    
+    explicit CamCalibration(int size);    
 
-private:
     std::vector<ImageCalibration> image_calibrations;
     MatVec lidar1_to_lidar2;
 };
 
 void read_mat3(std::istream &in, glm::mat3 &m);
-void print_mat3(const glm::mat3 &m);
+void print_mat3(std::ostream &os, const glm::mat3 &m);
 
 template <typename T>
-void read_vec(std::istream &in, T &vec, int size)
+std::istream& read_vec(std::istream &in, T &vec, int size)
 {
     for(int i = 0; i < size; ++i)
     {
         in >> vec[i];
     }
+
+    return in;
 }
 
 template <typename T>
-void print_vec(const T &vec, int size)
+std::ostream& print_vec(std::ostream &os, const T &vec, int size)
 {
     for(int i = 0; i < size; ++i)
     {
         std::cout << vec[i] << " ";
     }
     std::cout << "\n";
+
+    return os;
 }
 
 
+std::istream& operator>>(std::istream &in, MatVec &mv);
 std::istream& operator>>(std::istream &in, ImageCalibration &im);
+std::istream& operator>>(std::istream &in, CamCalibration &cam);
+std::ostream &operator<<(std::ostream &os, const MatVec &mv);
+std::ostream &operator<<(std::ostream &os, const ImageCalibration &im);
+std::ostream &operator<<(std::ostream &os, const CamCalibration &cam);
 
 #endif
